196725d01d25448b12ed7e694612ad9e3594dd8c
Janick Michot <michojan@students.zhaw.ch>
2019-07-02 13:00:51 +0200

    Add Calendar

--- frontend/src/app/tabs/calendar/calendar.page.ts
+++ frontend/src/app/tabs/calendar/calendar.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar/calendar';
+import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-calendar',
@@ -7,9 +10,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarPage implements OnInit {
 
-  constructor() { }
+    event = {
+        title: '',
+        desc: '',
+        startTime: '',
+        endTime: '',
+        allDay: false
+    };
 
-  ngOnInit() {
-  }
+    eventSource = [];
+    viewTitle;
 
+    calendar = {
+        mode: 'month',
+        currentDate: new Date(),
+    };
+
+    @ViewChild(CalendarComponent)
+    myCal: CalendarComponent;
+
+    constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) {
+    }
+
+    ngOnInit() {
+        this.resetEvent();
+    }
+
+    resetEvent() {
+        this.event = {
+            title: '',
+            desc: '',
+            startTime: new Date().toISOString(),
+            endTime: new Date().toISOString(),
+            allDay: false
+        };
+    }
+
+    // Change current month/week/day
+    next() {
+        const swiper = document.querySelector('.swiper-container')['swiper'];
+        swiper.slideNext();
+    }
+
+    back() {
+        const swiper = document.querySelector('.swiper-container')['swiper'];
+        swiper.slidePrev();
+    }
+
+    // Change between month/week/day
+    changeMode(mode) {
+        this.calendar.mode = mode;
+    }
+
+    // Focus today
+    today() {
+        this.calendar.currentDate = new Date();
+    }
+
+    // Calendar event was clicked
+    async onEventSelected(event) {
+
+      console.log("onEventSelected");
+
+        // Use Angular date pipe for conversion
+        const start = formatDate(event.startTime, 'medium', this.locale);
+        const end = formatDate(event.endTime, 'medium', this.locale);
+
+        const alert = await this.alertCtrl.create({
+            header: event.title,
+            subHeader: event.desc,
+            message: 'From: ' + start + '<br><br>To: ' + end,
+            buttons: ['OK']
+        });
+        alert.present();
+    }
+
+    // Time slot was clicked
+    onTimeSelected(ev) {
+        const selected = new Date(ev.selectedTime);
+        this.event.startTime = selected.toISOString();
+        selected.setHours(selected.getHours() + 1);
+        this.event.endTime = (selected.toISOString());
+
+        console.log("Time Clicked");
+    }
 }
